6d220f2a562242ed6af21fe35d2dc1cd89b92ec4
zrdouane <ramirdouane@gmail.com>
2023-08-29 14:57:24 +0100

    update task 16 & add binary_tree_print && add protype of binary_tree_print

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -64,15 +64,23 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t number_of_nodes = 0, height = 0;
+	int left_height = 0, right_height = 0;
 
-	if (tree == NULL)
+	if (!tree)
 		return (0);
-	number_of_nodes = binary_tree_nodes(tree);
-	height = binary_tree_height(tree);
-	if (number_of_nodes == 0)
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	if (left_height != right_height)
+		return (0);
+
+	if (!tree->left && !tree->right)
 		return (1);
-	if (number_of_nodes == (2 << height) - 1)
+
+	if (binary_tree_is_perfect(tree->left) &&
+	    binary_tree_is_perfect(tree->right))
 		return (1);
+
 	return (0);
 }
